9af16d352e077c0f15411a672215cbcb5515b9f4
arhim0use <chingisbek@gmail.com>
2025-01-29 19:47:01 +0300

    Todo TableView + DetailView created

---
+++ SimpleTodo/SimpleTodo/CoreData/TodoEntity.swift
@@ -0,0 +1,23 @@
+//
+//  TodoEntity.swift
+//  SimpleTodo
+//
+//  Created by Chingisbek Anvardinov on 29.01.2025.
+//
+
+//import CoreData
+
+//@objc(TodoEntity)
+//public class TodoEtity: NSManagedObject {
+//    @NSManaged public var id: Int
+//    @NSManaged public var title: String
+//    @NSManaged public var descr: String
+//    @NSManaged public var createdAt: Date
+//    @NSManaged public var completed: Bool
+//    @NSManaged public var userId: Int
+//    
+//    @nonobjc public class func fetchRequest() -> NSFetchRequest<TodoEntity> {
+//        NSFetchRequest<TodoEntity>(entityName: "TodoEntity")
+//    }
+//}
+
